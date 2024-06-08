8a7de159023bdc14a4256e4e085b4cb1012e1b63
Turppie <codar2004@hotmail.com>
2024-06-08 16:44:56 -0500

    feat: se implemento comunicacion de algunos componentes del modulo create-order

--- src/app/hacer-pedido/create-order/create-order.component.ts
+++ src/app/hacer-pedido/create-order/create-order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OrderRequest } from '../model/order-request.model';
 
 @Component({
   selector: 'app-create-order',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './create-order.component.css'
 })
 export class CreateOrderComponent {
+  order:OrderRequest={
+    shipping_address: '',
+    pickup_address: '',
+    express_shipping: false
+  };
 
+  onSelectEncomienda():void{
+    console.log('Seleccionar Tipo de Encomienda clicado');
+  }
+
+  onToggleExpressShipping(): void{
+    this.order.express_shipping=!this.order.express_shipping;
+    console.log('Envio Express activado: ', this.order.express_shipping);
+  }
+
+  onSubmitOrder(): void{
+    console.log('Pedido creado: ', this.order);
+  }
 }
