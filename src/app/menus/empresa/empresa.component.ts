358497980e48dd6c824a518826c2b5ca9a072088
DannRam <u202223632@upc.edu.pe>
2024-06-17 18:03:09 -0500

    feat: implemente los menus segun el tipo de usuario

---
+++ src/app/menus/empresa/empresa.component.ts
@@ -0,0 +1,23 @@
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empresa',
+  templateUrl: './empresa.component.html',
+  styleUrl: './empresa.component.css'
+})
+export class EmpresaComponent {
+  constructor(private Router: Router) { }
+
+  shipSummary(): void{
+    this.Router.navigate(['/hacer-pedido/create-order'])
+  }
+
+  shipAnalytics(): void{
+    this.Router.navigate(['/hacer-pedido/create-order'])
+  }
+
+  chats(): void{
+    this.Router.navigate(['/hacer-pedido/create-order'])
+  }
+}
